158af6b427ccea3734d3079ab1bcde8c8d799944
glebon0819 <g.lebon0819@gmail.com>
2017-06-29 17:58:49 -0700

    CSV class so far

---
+++ EasyCSV.php
@@ -0,0 +1,41 @@
+<?php
+class EasyCSV {
+	
+	//Takes a CSV file with one column of data and outputs an array of values
+	public static function verticalOutput($path) {
+		$array = file($path);
+			if(count(file($path)) == 0) {
+				throw new Exception ("CSV is empty");
+			}
+			foreach($array as $item) {
+				if(strpos($item, ",")) {
+					throw new Exception("CSV contains more than one column");
+				}
+			}
+		return $array;
+	}
+	
+	//Takes a CSV file with one row of data and outputs an array of values (if there are multiple rows, it only outputs the first row)
+	public static function horizontalOutput($path) {
+		$array = explode(",",file($path)[0]);
+		if(count(file($path)) == 0) {
+			throw new Exception ("CSV is empty");
+		}
+		
+		return $array;
+	}
+	
+	//Takes a CSV file with multiple rows/columns and outputs a 2D array of values
+	public static function twoDimensionalOutput($path) {
+		$array = self::verticalOutput($path);
+		foreach($array as &$sub) {
+			$sub = explode(",", $sub);
+		}
+		if(count(file($path)) == 0) {
+			throw new Exception ("CSV is empty");
+		}
+		return $array;
+	}
+	
+}
+?>
